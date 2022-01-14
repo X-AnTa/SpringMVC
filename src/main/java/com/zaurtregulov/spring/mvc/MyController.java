0f00aaf0b252a5639d4db00e55ccdd7ae28e2849
X-AnTa <mrspbusdriver@gmail.com>
2022-01-14 12:48:56 +0600

    SpringMVC_Course

---
+++ src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -0,0 +1,38 @@
+package com.zaurtregulov.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String showFirstView() {
+        return "first-view";
+    }
+
+    @RequestMapping("/askDetails")
+    public String askEmployeeDetails(Model model) {
+
+        model.addAttribute("employee", new Employee());
+
+        return "ask-emp-details-view";
+    }
+
+    @GetMapping("/showDetails")
+    public String showEmpDetails(@Valid @ModelAttribute("employee") Employee emp, BindingResult bindingResult) {
+
+
+        if (bindingResult.hasErrors()){
+            return "ask-emp-details-view";
+        }
+        else {
+            return "show-emp-details-view";
+        }
+    }
+}
